2de53c0f748c938fd450961bf89c34c25f93280c
karumajohn <karumajohn252203@gmail.com>
2020-04-08 06:10:07 +0300

    exit once

--- app/src/main/java/com/github/ppartisan/simplealarms/ui/MainActivity.java
+++ app/src/main/java/com/github/ppartisan/simplealarms/ui/MainActivity.java
@@ -1,16 +1,28 @@
 package com.github.ppartisan.simplealarms.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.github.ppartisan.simplealarms.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.e(TAG, "onBackPressed: " );
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory( Intent.CATEGORY_HOME );
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+    }
 }
